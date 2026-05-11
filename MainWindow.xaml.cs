87a3377448211c50bad2a63e6472c98e2cbe6781
Winnie <epanovaalina@gmail.com>
2026-05-11 10:44:00 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Distance_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка ввода
+            bool ok1 = double.TryParse(X1.Text, out double x1);
+            bool ok2 = double.TryParse(Y1.Text, out double y1);
+            bool ok3 = double.TryParse(X2.Text, out double x2);
+            bool ok4 = double.TryParse(Y2.Text, out double y2);
+
+            if (!ok1 || !ok2 || !ok3 || !ok4)
+            {
+                MessageBox.Show("Ошибка ввода координат!");
+                return;
+            }
+
+            MyPoint p1 = new MyPoint(x1, y1);
+            MyPoint p2 = new MyPoint(x2, y2);
+
+            double distance = p1.DistanceTo(p2);
+
+            Output.Text = "Расстояние: " + distance;
+        }
+
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка ввода
+            bool ok1 = double.TryParse(X1.Text, out double x1);
+            bool ok2 = double.TryParse(Y1.Text, out double y1);
+            bool ok3 = double.TryParse(X2.Text, out double x2);
+            bool ok4 = double.TryParse(Y2.Text, out double y2);
+
+            if (!ok1 || !ok2 || !ok3 || !ok4)
+            {
+                MessageBox.Show("Ошибка ввода координат!");
+                return;
+            }
+
+            MyPoint p1 = new MyPoint(x1, y1);
+            MyPoint p2 = new MyPoint(x2, y2);
+
+            ComboBoxItem item = (ComboBoxItem)OperationBox.SelectedItem;
+            string op = item.Content.ToString();
+            string result = "";
+
+            if (op == "X ++")
+            {
+                p1++;
+                result = p1.ToString();
+            }
+
+            else if (op == "X --")
+            {
+                p2--;
+                result = p2.ToString();
+            }
+
+            else if (op == "Расстояние")
+            {
+                double d = p1 + p2;
+                result = "Расстояние: " + d;
+            }
+
+            else if (op == "X + число")
+            {
+                bool ok = int.TryParse(NumberBox.Text, out int num);
+
+                if (!ok)
+                {
+                    MessageBox.Show("Введите число");
+                    return;
+                }
+
+                MyPoint res = p1 + num;
+
+                result = res.ToString();
+            }
+
+            else if (op == "Число + X")
+            {
+                bool ok = int.TryParse(NumberBox.Text, out int num);
+
+                if (!ok)
+                {
+                    MessageBox.Show("Введите число");
+                    return;
+                }
+
+                MyPoint res = num + p1;
+
+                result = res.ToString();
+            }
+
+            else if (op == "Целая часть от X int(x)")
+            {
+                int val = (int)p1;
+
+                result = val.ToString();
+            }
+
+            else if (op == "Координата Y double(x)")
+            {
+                double val = p1;
+
+                result = val.ToString();
+            }
+            Output.Text = result;
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+internal class MyPoint
+{
+    private double _x;
+    private double _y;
+
+    public MyPoint()
+    {
+        _x = 0;
+        _y = 0;
+    }
+
+    public MyPoint(double x, double y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public MyPoint(MyPoint other)
+    {
+        _x = other._x;
+        _y = other._y;
+    }
+
+    public double X
+    {
+        get { return _x; }
+        set { _x = value; }
+    }
+
+    public double Y
+    {
+        get { return _y; }
+        set { _y = value; }
+    }
+
+    public double DistanceTo(MyPoint other)
+    {
+        double dx = other._x - _x;
+        double dy = other._y - _y;
+
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    public static MyPoint operator ++(MyPoint p)
+    {
+        return new MyPoint(p._x + 1, p._y);
+    }
+
+    public static MyPoint operator --(MyPoint p)
+    {
+        return new MyPoint(p._x - 1, p._y);
+    }
+
+    public static explicit operator int(MyPoint p)
+    {
+        return (int)p._x;
+    }
+
+    public static implicit operator double(MyPoint p)
+    {
+        return p._y;
+    }
+
+    public static double operator +(MyPoint a, MyPoint b)
+    {
+        return a.DistanceTo(b);
+    }
+
+    public static MyPoint operator +(MyPoint p, int value)
+    {
+        return new MyPoint(p._x + value, p._y);
+    }
+
+    public static MyPoint operator +(int value, MyPoint p)
+    {
+        return new MyPoint(p._x + value, p._y);
+    }
+
+    public override string ToString()
+    {
+        return "Точка (" + _x + "; " + _y + ")";
+    }
+}
